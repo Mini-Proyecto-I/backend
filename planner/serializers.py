403d7fd1c0e71ed455174ec8f1da8aa61f4015d6
Jhoan Fernandez <fernandez.jhoan@correunivalle.edu.co>
2026-02-23 18:48:21 -0500

    Feature: validate activity

--- planner/serializers.py
+++ planner/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Course, Activity, Subtask, ReprogrammingLog
+from django.utils import timezone
 
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
@@ -14,7 +15,32 @@ class ActivitySerializer(serializers.ModelSerializer):
         model = Activity
         fields = ["id", "title", "description", "course", "course_id",
                   "user", "created_at", "event_datetime", "deadline"]
-        extra_kwargs = {"user": {"required": True}}
+        extra_kwargs = {
+            "user": {"required": True},
+            "title": {"required": True, "allow_blank": False}
+            }
+        
+        #Validacion titulo
+    def validate_title(self, value):
+        if not value.strip():
+            raise serializers.ValidationError("El título no puede estar vacío.")
+        return value
+    
+    #Validación global
+    def validate(self, data):
+        event_datetime = data.get("event_datetime")
+        deadline = data.get("deadline")
+        if event_datetime and event_datetime < timezone.now():
+            raise serializers.ValidationError({
+                "event_datetime": "La fecha de la actividad no puede ser anterior a la actual."
+            })
+        if event_datetime and deadline:
+            if deadline < event_datetime.date():
+                raise serializers.ValidationError({
+                    "deadline": "La fecha límite de la actividad no puede ser anterior a la actual."
+            })
+        return data
+
 
     def create(self, validated_data):
         validated_data["course"] = validated_data.pop("course_id", None)
@@ -34,7 +60,20 @@ class SubtaskSerializer(serializers.ModelSerializer):
         model = Subtask
         fields = ["id", "title", "activity", "activity_id", "user", "status", "estimated_hours",
                   "target_date", "order", "is_conflicted", "execution_note"]
-        extra_kwargs = {"user": {"required": True}}
+        extra_kwargs = {
+            "user": {"required": True},
+            "title": {"required": True, "allow_blank": False}
+        }
+
+    def validate_title(self, value):
+        if not value.strip():
+            raise serializers.ValidationError("El título de la subtarea no puede estar vacío.")
+        return value
+    
+    def validate_estimated_hours(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Las horas estimadas deben ser mayores a 0.")
+        return value
 
     def create(self, validated_data):
         validated_data["activity"] = validated_data.pop("activity_id")
